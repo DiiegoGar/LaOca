a2783b4a92c1875829a19c26f5f2532e38c960e0
DiiegoGar <trillisgc@gmail.com>
2021-04-24 12:41:21 +0200

    LaOca

---
+++ Dado.java
@@ -0,0 +1,23 @@
+package Parchis;
+import java.util.*;
+
+public class Dado {
+	private static Dado miDado;
+	Random al;
+	
+	private Dado() {
+		al = new Random();
+	}
+	
+	public static Dado getDado() {
+		if (Dado.miDado == null) {
+			Dado.miDado = new Dado();
+		}
+		return Dado.miDado;
+	}
+	
+	public int lanzarDado() {
+		int n = al.nextInt(6) + 1;
+		return n;
+	}
+}
